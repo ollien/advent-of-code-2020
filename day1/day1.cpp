c60a258bc2257814c10d34e4a8ce4881f5da2206
Nick Krichevsky <nick@ollien.com>
2020-12-01 00:42:34 -0500

    Reformat day1.cpp

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
-#include <set>
 
 constexpr int TARGET_NUM = 2020;
 
@@ -11,61 +11,50 @@ constexpr int TARGET_NUM = 2020;
  * @param filename The filename to read from
  * @return std::set<int> A set of the numbers in the input
  */
-std::set<int> read_input(const std::string &filename)
-{
-	std::set<int> input;
-	std::string line;
-	std::ifstream file(filename);
+std::set<int> read_input(const std::string &filename) {
+  std::set<int> input;
+  std::string line;
+  std::ifstream file(filename);
 
-	while (std::getline(file, line))
-	{
-		int num = std::stoi(line);
-		input.insert(num);
-	}
+  while (std::getline(file, line)) {
+    int num = std::stoi(line);
+    input.insert(num);
+  }
 
-	return input;
+  return input;
 }
 
-int part1(const std::set<int> &inputs)
-{
-	for (int num : inputs)
-	{
-		int desired = TARGET_NUM - num;
-		if (inputs.find(desired) != inputs.end())
-		{
-			return desired * num;
-		}
-	}
+int part1(const std::set<int> &inputs) {
+  for (int num : inputs) {
+    int desired = TARGET_NUM - num;
+    if (inputs.find(desired) != inputs.end()) {
+      return desired * num;
+    }
+  }
 
-	throw "Does not contain solution";
+  throw std::runtime_error("Does not contain solution");
 }
 
-int part2(const std::set<int> &inputs)
-{
-	for (int num : inputs)
-	{
-		for (int num2 : inputs)
-		{
-			int desired = TARGET_NUM - (num + num2);
-			if (inputs.find(desired) != inputs.end())
-			{
-				return desired * num2 * num;
-			}
-		}
-	}
+int part2(const std::set<int> &inputs) {
+  for (int num : inputs) {
+    for (int num2 : inputs) {
+      int desired = TARGET_NUM - (num + num2);
+      if (inputs.find(desired) != inputs.end()) {
+        return desired * num2 * num;
+      }
+    }
+  }
 
-	throw std::runtime_error("Does not contain solution");
+  throw std::runtime_error("Does not contain solution");
 }
 
-int main(int argc, const char *argv[])
-{
-	if (argc != 2)
-	{
-		std::cerr << "./day1 <input_file>" << std::endl;
-		return 1;
-	}
+int main(int argc, const char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "./day1 <input_file>" << std::endl;
+    return 1;
+  }
 
-	std::set<int> inputs = read_input(argv[1]);
-	std::cout << part1(inputs) << std::endl;
-	std::cout << part2(inputs) << std::endl;
+  std::set<int> inputs = read_input(argv[1]);
+  std::cout << part1(inputs) << std::endl;
+  std::cout << part2(inputs) << std::endl;
 }
